e31529af1e39d0372980971c7c181018baa0b2f9
ofirshitrit <ofirha18@gmail.com>
2023-08-15 23:31:41 +0300

    add os path do the PDF code

--- src/Histograms.py
+++ src/Histograms.py
@@ -29,20 +29,17 @@ def create_histogram(csv_file, output_folder, file_name,num_bins=50):
 
 # Get the parent directory of the current script (src folder)
 parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
 # Construct the path to the csvFiles folder
-folder_path = os.path.join(parent_directory, 'resources', 'csvFiles')
-
-
+csv_files = os.path.join(parent_directory, 'resources', 'csvFiles')
 # Navigate to the output folder within the res folder
 output_folder = os.path.join(parent_directory, 'res', 'HistogramsPlots')
 
 # List all files in the directory
-csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv') and file not in ['Noise.csv', 'Mix.csv']]
+csv_files = [file for file in os.listdir(csv_files) if file.endswith('.csv') and file not in ['Noise.csv', 'Mix.csv']]
 
 # Process each CSV file
 for csv_file in csv_files:
-    csv_path = os.path.join(folder_path, csv_file)
+    csv_path = os.path.join(csv_files, csv_file)
     # Extract the name of the file (without the extension)
     file_name = os.path.splitext(csv_file)[0]
     create_histogram(csv_path, output_folder,file_name)
--- src/PDF.py
+++ src/PDF.py
@@ -4,48 +4,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def pdf_plot(input_folder, output_folder, bins=10):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def custom_pdf_plot(csv_folder, output_dir, num_bins=10):
+    for filename in os.listdir(csv_folder):
+        if 'Noise' not in filename and filename.endswith('.csv'):
+            print(filename)
+            file_path = os.path.join(csv_folder, filename)
+            data_df = pd.read_csv(file_path)
+            data_df = data_df.drop(['No.'], axis=1)
+            data_df['Delay'] = pd.to_numeric(data_df['Time']).diff()
 
-    for filename in os.listdir(input_folder):
-        if filename != 'Noise' and filename.endswith('.csv'):
-            file_path = os.path.join(input_folder, filename)
-            df = pd.read_csv(file_path)
-            df = df.drop(['No.'], axis=1)
-            df['Delay'] = pd.to_numeric(df['Time']).diff()
+            clean_data = data_df.dropna(subset=['Delay'])
 
-            cleaned = df.dropna(subset=['Delay'])
-
-            counts, edges = np.histogram(cleaned['Delay'], bins=bins, density=True)
-
+            counts, edges = np.histogram(clean_data['Delay'], bins=num_bins, density=True)
             pdf = counts / sum(counts)
 
             plt.figure(figsize=(10, 6))
-
             plt.step(edges[:-1], pdf, where='post', color='blue', lw=2, label='PDF')
 
-            rate = 1. / np.mean(df['Delay'])
-            width = edges[1] - edges[0]
-            scale = max(pdf) / (rate * np.exp(-rate * edges[np.argmax(pdf)]))
+            mean_delay = 1. / np.mean(data_df['Delay'])
+            bin_width = edges[1] - edges[0]
+            scale_factor = max(pdf) / (mean_delay * np.exp(-mean_delay * edges[np.argmax(pdf)]))
 
             x = np.linspace(0, max(edges), 500)
-            y = scale * rate * np.exp(-rate * x)
+            y = scale_factor * mean_delay * np.exp(-mean_delay * x)
 
             plt.plot(x, y, color='red', lw=2, label='Fitted Exp')
 
             plt.xlabel('Inter-Arrival Time')
-            plt.ylabel('Density')
-            plt.title('PDF of Inter-Arrival Time and Fitted Exp')
+            plt.ylabel('PDF')
+            plt.title('Inter-Arrival Time PDF and Fitted Exp')
             plt.legend()
 
             output_filename = os.path.splitext(filename)[0] + '_plot.png'
-            output_path = os.path.join(output_folder, output_filename)
+            output_path = os.path.join(output_dir, output_filename)
             plt.savefig(output_path)
-
             plt.close()
 
 
-input_folder = '/home/ofr/PycharmProjects/FinalProject/resources/csvFiles'
-output_folder = '/home/ofr/PycharmProjects/FinalProject/res/PDFPlots'
-pdf_plot(input_folder,output_folder)
+# Get the parent directory of the current script
+script_parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Define paths for CSV files and output plots
+csv_files_folder = os.path.join(script_parent_dir, 'resources', 'csvFiles')
+output_plots_folder = os.path.join(script_parent_dir, 'res', 'PDFPlots')
+
+# Call the custom_pdf_plot function
+custom_pdf_plot(csv_files_folder, output_plots_folder)
